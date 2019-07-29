6fe982ba4827603d921d36f4df0b3692f558dccf
Erin Stalzer <erinstalzer@gmail.com>
2019-07-29 16:17:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+class Driver{
+    constructor(name, date){
+        this.name = name
+        this.date = date
+    }
 
+    startDate(){
+        return new Date(this.date)
+    }
+
+    yearsExperienceFromBeginningOf(endDate){
+        console.log(startDate())
+        // return endDate - this.startDate().year
+    }
+}
+
+const eastWest = [
+    '1st Avenue',
+    '2nd Avenue',
+    '3rd Avenue',
+    'Lexington Avenue',
+    'Park',
+    'Madison Avenue',
+    '5th Avenue'
+  ]
+
+  let begArr
+  let endArr
+
+class Route{
+    constructor(beginningLocation, endingLocation){
+        this.beginningLocation = beginningLocation
+        this.endingLocation = endingLocation
+    }
+
+    blocksTraveled(){
+        begArr = beginningLocation.split(" and ")
+        endArr = endingLocation.split(" and ")
+        console.log(begArr)
+    console.log(endArr)
+    }
+
+    estimatedTime(){
+
+    }    
+
+}
