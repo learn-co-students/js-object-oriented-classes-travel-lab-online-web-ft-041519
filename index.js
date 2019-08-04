79781f9d0602924099689b1ebf5d444377462aee
Alison Golden <alisong779@yahoo.com>
2019-08-04 14:54:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  yearsExperienceFromBeginningOf(year){
+    return year - this.startDate.getFullYear()
+  }
+}
 
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+    this.eastWest = [
+      '1st Avenue',
+      '2nd Avenue',
+      '3rd Avenue',
+      'Lexington Avenue',
+      'Park',
+      'Madison Avenue',
+      '5th Avenue'
+    ];
+  }
+  blocksTravelled() {
+        const verticalDist = Math.abs(this.endingLocation["vertical"] - this.beginningLocation["vertical"]);
+        const horizDist = Math.abs(this.eastWest.indexOf(this.endingLocation["horizontal"]) - this.eastWest.indexOf(this.beginningLocation["horizontal"]));
+        return verticalDist + horizDist;
+    }
+  estimatedTime(peak){
+    let time = this.blocksTravelled()
+    if (peak)
+      return (time / 2)
+    else
+    return (time / 3)
+  }
+}
