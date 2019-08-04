a17099350c2848397c44e31eea239eee8beb386d
Kish-Siva-1 <rodes88@msn.com>
2019-08-04 18:58:07 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
+class Driver {
+    constructor(name, date) {
+      this.name = name;
+      this.date = date;
+    }
 
+    yearsExperienceFromBeginningOf(current) {
+        let dates = this.date; 
+        return current - dates.slice(-4) ; 
+    }
+   
+  }
+
+class Route {
+    constructor(beglocation, endinglocation) {
+        this.beglocation = beglocation;
+        this.endinglocation = endinglocation;
+      } 
+
+      blocksTravelled () {
+        
+        let eastWest = [
+            '1st Avenue',
+            '2nd Avenue',
+            '3rd Avenue',
+            'Lexington Avenue',
+            'Park',
+            'Madison Avenue',
+            '5th Avenue'
+          ];
+        
+        const horizontal_distance = Math.abs(eastWest.indexOf(this.beglocation.horizontal) - eastWest.indexOf(this.endinglocation.horizontal));  
+        const vertical_distance = Math.abs(this.beglocation.vertical - this.endinglocation.vertical);    
+        
+        return horizontal_distance + vertical_distance; 
+
+      }
+
+      estimatedTime (input) {
+          if (input === true) 
+            return this.blocksTravelled() / 2
+          else 
+            return this.blocksTravelled() / 3 
+      }
+      
+} 
